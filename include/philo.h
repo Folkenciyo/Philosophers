a9d007a80f7684a0e6d46a504babe34a8c989f5a
Folkenciyo <folkencillo@gmail.com>
2024-01-17 18:24:35 +0100

    auto commit

---
+++ include/philo.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juguerre <juguerre@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 18:07:39 by juguerre          #+#    #+#             */
+/*   Updated: 2024/01/17 18:07:39 by juguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <string.h>
+
+
+/**** Error Functions ****/
+void		error(char *error);
+
+#endif
---
+++ libft/src/ft_lst/ft_lstiter_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juguerre <juguerre@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 00:41:01 by juguerre          #+#    #+#             */
+/*   Updated: 2023/12/05 18:41:41 by juguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
